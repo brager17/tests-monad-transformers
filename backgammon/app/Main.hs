2fe81f4740fa8692d540b51d16742cd3e414f7b1
Evgeny Terekhin <eter55@yandex.ru>
2019-11-10 12:16:02 +0300

    new framework

--- backgammon/app/Main.hs
+++ backgammon/app/Main.hs
@@ -1,6 +1,70 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE KindSignatures #-}
+
+{-# LANGUAGE PolyKinds          #-}
+{-# OPTIONS_HADDOCK not-home    #-}
+
 module Main where
 
-import Server
+import Prelude ()
+import Prelude.Compat
+
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Attoparsec.ByteString
+import Data.ByteString (ByteString)
+import Data.List
+import Data.Maybe
+import Data.String.Conversions
+import Data.Time.Calendar
+import GHC.Generics
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import System.Directory
+import Servant.Types.SourceT (source)
+
+type UserAPI1 = "users" :> Get '[JSON] [User]
+
+data User = User {
+  name :: String,
+  age :: Int,
+  email :: String
+} deriving (Eq, Show, Generic)
+instance ToJSON User
+
+users1 :: [User]
+users1 =
+  [ User "Isaac Newton"    372 "isaac@newton.co.uk" 
+  , User "Albert Einstein" 136 "ae@mc2.org"         
+  ]
+
+server1 :: Server UserAPI1
+server1 = return users1
+
+userAPI :: Proxy UserAPI1
+userAPI = Proxy
+
+-- 'serve' comes from servant and hands you a WAI Application,
+-- which you can think of as an "abstract" web application,
+-- not yet a webserver.
+app1 :: Application
+app1 = serve userAPI server1
 
 main :: IO ()
-main = listen
+main = run 8081 app1
+
+
+data (path :: k) ++ (a :: *)
+
--- backgammon/src/Server.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Server(listen) where
-
-import Snap.Core (ifTop,writeText)
-import Snap.Http.Server (quickHttpServe)
-
-listen :: IO()
-listen = quickHttpServe $ ifTop (writeText "Backgammon")
